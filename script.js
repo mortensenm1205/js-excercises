c8aa316d494fd63f499d5aabaaea1d4716572d88
mortensenm1205@gmail.com <mtt.mortensen527@gmail.com>
2017-11-29 10:17:04 -0700

    okay my problem with my Name element was it's var name, renamed to fname. Wrote function that just works with focusin events. Chagned backgroundColor to pink when focusin

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-var name = document.getElementById("field1");
+var fname = document.getElementById("field1");
 var email = document.getElementById("field2");
 var phoneNumber = document.getElementById("field3");
 var btn = document.getElementById("btn");
 
-btn.addEventListener('click', function() {
-  console.log("This Worked");
-});
+function focusInput(event) {
+  event.addEventListener('focusin', function() {
+    this.style.backgroundColor = "pink";
+  })
+}
 
-email.addEventListener('focusin', function() {
-  console.log("This works with focus");
-});
+focusInput(fname);
+focusInput(email);
+focusInput(phoneNumber);
