cd024c0bc6c8dfe394f01130758e413b7c90e37c
mortensenm1205@gmail.com <mtt.mortensen527@gmail.com>
2017-11-29 09:50:15 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var name = document.getElementById("field1");
+var email = document.getElementById("field2");
+var phoneNumber = document.getElementById("field3");
+var btn = document.getElementById("btn");
+
+btn.onclick = function() {
+  console.log("I guess this works as well");
+}
