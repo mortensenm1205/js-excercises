01002eed0b500a879baca94051ce0e4aa49c518d
mortensenm1205@gmail.com <mtt.mortensen527@gmail.com>
2017-11-29 11:51:04 -0700

    moved my form-validator to a new dir, form-valid

---
+++ form-valid/script.js
@@ -0,0 +1,27 @@
+var fname = document.getElementById("field1");
+var email = document.getElementById("field2");
+var phoneNumber = document.getElementById("field3");
+var btn = document.getElementById("btn");
+var result = document.getElementById("result");
+
+var arrInput = [fname, email, phoneNumber]
+
+function formValid(event) {
+  if (event.value !== '') {
+    result.innerHTML += event.value + ' ';
+  }
+  event.value = '';
+}
+
+btn.addEventListener('click', function() {
+  if (arrInput[0].value === '' && arrInput[1].value === '' && arrInput[2].value === '') {
+    alert('Please fill in all fields');
+  }
+
+  for(var i = 0; i < arrInput.length; i++) {
+      formValid(arrInput[i]);
+  }
+  // formValid(fname);
+  // formValid(email);
+  // formValid(phoneNumber);
+});
