8d829e10ceb9d3e5e7d6a1be87a647d017ae58c9
mortensenm1205@gmail.com <mtt.mortensen527@gmail.com>
2017-11-29 09:55:23 -0700

    eventListener placed on btn, waiting for click, simple funcitonto the console

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ var email = document.getElementById("field2");
 var phoneNumber = document.getElementById("field3");
 var btn = document.getElementById("btn");
 
-btn.onclick = function() {
-  console.log("I guess this works as well");
-}
+btn.addEventListener('click', function() {
+  console.log("This Worked");
+});
