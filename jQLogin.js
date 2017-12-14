063f4f252eaaf5a59c7bd6400d9ede2f6f74948e
mortensenm1205@gmail.com <mtt.mortensen527@gmail.com>
2017-12-14 16:54:35 -0700

    wrote comments going over my thought process and what is going on

--- jQLogin.js
+++ jQLogin.js
@@ -12,10 +12,13 @@ $(function() {
 
   $circle.click(function() {
     $(this).slideUp(4000, function() {
+      //The background willl change and it will append this new text AFTER the 4000 secs are done.
       $(this).css('background-color', 'pink');
       $(this).append(function() {
+        //We're creating a new el and then maniuplating it's text/style with jQ methods
         var $txt = $('<p></p>');
         $txt.text('hard coded text').css({'margin': '0', 'padding': '45% 5%', 'color': 'white', 'font-size': '24px'});
+        //Anytime we have to deal with a method that only takes a callback or another value and not both then we have to return a value to the callback.
         return $txt;
       });
     });
