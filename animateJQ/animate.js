13a5474bc2cdd6337fb3bf04bfaace4fc8632ccb
mortensenm1205@gmail.com <mtt.mortensen527@gmail.com>
2017-12-15 12:25:45 -0700

    added a h1 title within jQ that also has animated jQ values

--- animateJQ/animate.js
+++ animateJQ/animate.js
@@ -1,13 +1,34 @@
 $(function() {
 
+  //Elements setup, through html elements
   var $imgContainer = $('#img-container');
   var $img = $('img');
   var $x = $('#x');
 
+  //Here's the title element I wanted to create, give it content, and then append all with jQ
+  var $title = $('<h1></h1>');
+  $title.text("Hello There! Welcome to the wonderful world of jQuery <3");
+  $('body').append($title);
+
+  //updating the css for $title
+  $title.css({
+    'max-width': '1000px',
+    'position': 'absolute',
+    'left': '23%',
+    'font-size': '4em',
+    'color': 'white'
+  });
+
+  //setting a value for button X but with jQ
   $x.val('✓')
+  //Same here but with imgContainer and it's position
+  $imgContainer.css({
+    'position': 'absolute',
+    'left': '-600px'
+  });
 
-  $imgContainer.css({'position': 'absolute', 'left': '-600px'});
-
+  //The main events.
+  //Had to use animate since slideUp/Down changed animation after I changed my picture size
   $x.click(function() {
     if($imgContainer.css('left') == '-600px' ) {
       $x.val('X')
@@ -21,5 +42,10 @@ $(function() {
       });
     }
   });
+
+  //Animating title to appear and then disappear
+  $title.hide();
+  $title.fadeIn(4000);
+  $title.fadeOut(2000);
 
 });
